1935e378f5ef93bacb90f09200d34c681c4491d5
Praveen de Silva <137995767+praveendesilvaa@users.noreply.github.com>
2024-03-23 17:06:24 +0530

    join us 2nd

--- EduVista/src/JoinUsNow.jsx
+++ EduVista/src/JoinUsNow.jsx
@@ -1,15 +1,16 @@
-// JoinUsNow.jsx
-
 import React from 'react';
 import './JoinUsNow.css';
-import joinUsImage from './assets/Educational-Websites.png'; 
+import joinUsVideo from './assets/pexels_videos_3718 (1080p).mp4'; 
 
 function JoinUsNow() {
   return (
     <div className="join-us-container">
       <div className="join-us-content">
         <div className="join-us-image-container">
-          <img src={joinUsImage} alt="Join Us" className="join-us-image" />
+          <video autoPlay muted loop className="join-us-image">
+            <source src={joinUsVideo} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
         </div>
         <div className="join-us-text">
           <h1 className="join-us-heading">Join Us Now</h1>
