ed0e05bce39d880e84bbde9cb733cc43aa551fc8
Praveen de Silva <137995767+praveendesilvaa@users.noreply.github.com>
2024-03-23 16:51:11 +0530

    courseDetails part 2

--- EduVista/src/CourseDetails.jsx
+++ EduVista/src/CourseDetails.jsx
@@ -21,3 +21,26 @@ function CourseDetails({ title, price, level, numReviews, numSubscribers, publis
   const randomPublishedDate = new Date(
     new Date() - Math.floor(Math.random() * 100) * 24 * 60 * 60 * 1000
   ).toLocaleDateString();
+
+  return (
+    <div className="course-details-dialog left-position"> 
+      <div className="course-details-content">
+        <p className="title">{title}</p> 
+        <hr /> 
+        <div className={`info`}>
+          <div className={`level-box ${levelClassName}`}>
+            <p className="level">{level}</p> 
+          </div>
+          <p>{numReviews} reviews</p>
+          <p className="price">Price: {price}</p>
+        </div>
+        <p>{numSubscribers} subscribers</p> 
+        <p>Published Date: <span className="green-date">{publishedDate || randomPublishedDate}</span></p> 
+        {/* Call handleAddToCart function when the button is clicked */}
+        <button className="add-to-cart-button" onClick={handleAddToCart}>Add to Cart</button> 
+      </div>
+    </div>
+  );
+}
+
+export default CourseDetails;
